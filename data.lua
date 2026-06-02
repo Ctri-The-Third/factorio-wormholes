08c6f8fd37ebd6364568c154add2844bffa405bc
jahangmar <48703856+jahangmar@users.noreply.github.com>
2026-06-02 17:48:13 +0200

    remove fire-armour, fix wormhole sprite showing for visible planets mod

--- data.lua
+++ data.lua
@@ -13,57 +13,7 @@ local victory_sprite = {
 }
 data.extend{victory_sprite}
 
-local fireArmour = table.deepcopy(data.raw["armor"]["heavy-armor"])
+if mods["visible-planets"] then
+    vp_add_planets_to_blacklist({"wormhole";})
+end 
 
-
-fireArmour.name = "fire-armour"
-fireArmour.icons = {
-    {
-        icon = fireArmour.icon,
-        icon_size = fireArmour.icon_size,
-        tint = {r=1, g=0, b=0, a=0.3}
-    },
-}
-
-
-
-fireArmour.resistances = {
-  {
-    type = "physical",
-    decrease = 6,
-    percent = 10
-  },
-  {
-    type = "explosion",
-    decrease = 10,
-    percent = 30
-  },
-  {
-    type = "acid",
-    decrease = 5,
-    percent = 30
-  },
-  {
-    type = "fire",
-    decrease = 0,
-    percent = 100
-  }
-}
-
-local recipe = {
-    type = "recipe",
-    name="fire-armour",
-    enabled=true,
-    energy_required = 8,
-    ingredients = {
-        {type="item", name = "copper-plate", amount = 200},
-        {type="item", name = "steel-plate", amount = 50}
-    },
-    results = {{type="item", name="fire-armour",amount=1}}
-
-
-}
-
-
-
-data:extend{fireArmour, recipe}
